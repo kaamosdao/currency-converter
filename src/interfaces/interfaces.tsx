55701a9c121f44daa45b53d8ff687957dfcf8ffb
Alexander <kaamosdao@gmail.com>
2022-11-01 19:02:37 +0300

    common convertation logic and ui

--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -6,10 +6,4 @@ export interface IRoutes {
   readonly [routeName: string]: string;
 }
 
-export interface IConvertResult {
-  readonly from: string;
-  readonly to: string;
-  readonly amount: number;
-}
-
 export default IRate;
--- src/pages/ConvertationPage.tsx
+++ src/pages/ConvertationPage.tsx
@@ -1,27 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import routes from '../routes';
-import { IConvertResult } from '../interfaces/interfaces';
 
 const Convertation: React.FC = () => {
-  const [convertResult, setConvertResult] = useState<IConvertResult | null>(null);
+  const [result, setResult] = useState<number | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const onSubmit = () => {
-    const convertCurrency = async (from: string, to: string, amount: string) => {
-      const response = await axios.get(routes.convert, {
-        params: { from, to, amount },
-        headers: { apikey: '638MYx3XDtSkA31Ow406rVqKW0bdeGpl' },
-      });
-      setConvertResult({ from, to, amount: response.data.result });
+  const convertCurrency = async (from: string, to: string, amount: string) => {
+    const response = await axios.get(routes.convert, {
+      params: { from, to, amount },
+      headers: { apikey: '638MYx3XDtSkA31Ow406rVqKW0bdeGpl' },
+    });
+    return response.data.result;
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const setCurrency = async (from: string, to: string, amount: string) => {
+      const convertedResult = await convertCurrency(from, to, amount);
+      setResult(convertedResult);
     };
-    // convertCurrency();
+
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      readonly converter: { readonly value: string };
+    };
+    const values = target.converter.value.trim().split(' ');
+
+    setCurrency(values[1], values[3], values[0]);
   };
 
   useEffect(() => {
     console.log('render ConvertationPage');
   });
 
-  return <p>Convertation</p>;
+  return (
+    <div className="d-flex flex-column">
+      <form className="row g-3 align-items-center" onSubmit={onSubmit}>
+        <div className="col-auto">
+          <label htmlFor="converter" className="col-form-label">
+            Converter
+          </label>
+        </div>
+        <div className="col-auto">
+          <input
+            ref={inputRef}
+            type="text"
+            className="form-control"
+            id="converter"
+            name="converter"
+            placeholder="15 usd in eur"
+          />
+        </div>
+        <div className="col-auto">
+          <button type="submit" className="btn btn-primary ms-3">
+            Convert
+          </button>
+        </div>
+      </form>
+      <p className="result">{result}</p>
+    </div>
+  );
 };
 
 export default Convertation;
