9c94545393ae2ee68fbd4c2d9f5ef352e30bb5a0
Alexander <kaamosdao@gmail.com>
2022-11-08 13:24:30 +0300

    install nock

--- src/hooks/HttpClientContext.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { IMakeRequest } from '../interfaces/interfaces';
-
-const HttpClientContext = React.createContext<IMakeRequest>({} as IMakeRequest);
-
-export default HttpClientContext;
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,14 +1,10 @@
 import { useContext } from 'react';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../slices/index';
-import { IMakeRequest, ILocalStorageData } from '../interfaces/interfaces';
-import HttpClientContext from './HttpClientContext';
+import { ILocalStorageData } from '../interfaces/interfaces';
 import LocalStorageContext from './localStorageContext';
 
-const useHttp = () => useContext<IMakeRequest>(HttpClientContext);
 export const useLocalStorage = () => useContext<ILocalStorageData>(LocalStorageContext);
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export default useHttp;
