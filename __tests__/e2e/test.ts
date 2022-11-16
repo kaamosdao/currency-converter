ff60c7fe1d71a7ef756f5c4506da3d4af895c4e9
Alexander <kaamosdao@gmail.com>
2022-11-16 14:20:33 +0300

    add text for currency convertation, remove nock

--- __tests__/e2e/test.ts
+++ __tests__/e2e/test.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { test, expect } from '@playwright/test';
+import routes from '../../src/utils/routes';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4242/');
@@ -15,4 +17,30 @@ test('page elements', async ({ page }) => {
   await page.click('.link-exchange');
   await expect(page.locator('.btn')).toHaveText('Change');
   await expect(page.locator('.form-control')).toHaveId('currency');
+});
+
+test.describe('good requests, data handling', () => {
+  test('currency convertation', async ({ page }) => {
+    await page.route(`${routes.convert}**`, (route) => {
+      setTimeout(() => {
+        route.fulfill({
+          body: JSON.stringify({
+            result: 1.64123,
+            success: true,
+          }),
+        });
+      }, 2000);
+    });
+    page.on('request', (request) => {
+      console.log(request.url());
+    });
+    await page.fill('.form-control', '100 rub in usd');
+    await page.click('.btn');
+    await expect(page.getByRole('status')).toHaveClass('spinner-border text-primary');
+    await expect(page.locator('.spinner-border')).toHaveText('Loading...');
+    await expect(page.locator('.result')).toHaveText(
+      'Convertation: 100 RUB in USD = 1.64123',
+      { timeout: 3000 },
+    );
+  });
 });
